eaa4a4603265297135f91a2acbca92af66814eff
ggggggggithub <120395149+ggggggggithub@users.noreply.github.com>
2023-10-02 20:23:17 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,17 @@
+function solution(hp) {
+    var answer = 0;
+    if (hp>=5){
+         answer += Math.floor(hp/5)
+        hp = (hp%5)
+       
+    }
+    if (hp<5 && hp>=3){
+        answer += Math.floor(hp/3)
+        hp = (hp%3)
+  
+    }
+    if (hp<3){
+        answer += Math.floor(hp/1)
+    }
+    return answer;
+}
