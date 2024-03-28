82623b0047c2da6d31665635b6caca31d3a0f603
ggggggggithub <120395149+ggggggggithub@users.noreply.github.com>
2024-03-28 21:39:32 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    var answer = 0;
+    answer = 1
+    for(let i=0; i<arr.length; i++){
+        for(let j=0; j<arr[i].length; j++){
+            if (arr[i][j] !== arr[j][i]){
+                answer = 0
+                break;
+            }
+        }
+    }
+    return answer;
+}
